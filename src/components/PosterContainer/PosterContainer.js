688dc8cf905a4bfa9d7eb79c24eacc527cafcea0
alexander215 <alexander.hoskinson@colorado.edu>
2020-06-21 14:38:19 -0700

    Basic hook set up in poster container

--- src/components/PosterContainer/PosterContainer.js
+++ src/components/PosterContainer/PosterContainer.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const PosterContainer = () => {
+  const [generate, setGenerate] =  useState('Nothing here')
   return (
     <div>
       This is the poster container.
+      <div onClick={() => setGenerate('Here is your picture')}>Generate</div>
+      {generate}
     </div>
   )
 }
