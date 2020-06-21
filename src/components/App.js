86b84c4e8f26f346e320134f7ad8ead9039a4f35
alexander215 <alexander.hoskinson@colorado.edu>
2020-06-21 14:24:05 -0700

    Create poster container div

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import PosterContainer from './PosterContainer/PosterContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Motivation Generation</h1>
+        <PosterContainer />
       </header>
     </div>
   );
---
+++ src/components/PosterContainer/PosterContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PosterContainer = () => {
+  return (
+    <div>
+      This is the poster container.
+    </div>
+  )
+}
+
+export default PosterContainer;
