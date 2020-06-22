2e22314fb6c3291755f27bc6f8f7384c47c44000
alexander215 <alexander.hoskinson@colorado.edu>
2020-06-22 00:06:13 -0700

    Toggle photo container

--- src/components/PosterContainer/PosterContainer.js
+++ src/components/PosterContainer/PosterContainer.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react'
 import PhotoContainer from '../PhotoContainer/PhotoContainer'
+import { getRoles } from '@testing-library/react'
 
 const PosterContainer = () => {
   const [generate, setGenerate] =  useState(false)
   const [image, setImage] = useState(null)
 
+  let displayPhoto = null;
+  if (generate) { displayPhoto =  <PhotoContainer />}
+
 
   return (
     <div>
       This is the poster container.
-      <div onClick={() => setGenerate(true)}>Generate</div>
-      <PhotoContainer />
+      <div onClick={() => setGenerate(!generate)}>Generate</div>
+      {displayPhoto}
     </div>
   )
 }
